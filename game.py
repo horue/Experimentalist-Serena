b49b147583f9b7f502dcc23fdb31d4bb07fd5d9f
Jorge <jorgejapao00@gmail.com>
2024-06-17 11:49:30 -0300

    Logic to update stages and read save file added

--- game.py
+++ game.py
@@ -11,4 +11,13 @@ def save_game(r):
     with open('save.ini', 'w') as configfile:
         save.write(configfile)
 
-save_game(8)
+def save_status(stage, status):
+    save[f'{stage}'] = {'satatus' : f'{status}'}
+    with open('save.ini', 'w') as configfile:
+        save.write(configfile)
+
+def read_save_file():
+    print(save['level']['i'])
+
+
+read_save_file()
