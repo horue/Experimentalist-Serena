48d14e32dd7f408e3c6979ec0692214c8c141479
Jorge <jorgejapao00@gmail.com>
2024-06-18 08:51:05 -0300

    Continue function updated

--- save.py
+++ save.py
@@ -5,28 +5,38 @@ import configparser
 save = configparser.ConfigParser()
 save.read('save.ini')
 
-
+## Used to save the game ##
 def save_game(r):
     save['level'] = {'i':f'{r}'}
     with open('save.ini', 'w') as configfile:
         save.write(configfile)
 
+## Used to check stage status ##
 def save_status(stage, status):
     save[f'{stage}'] = {'satatus' : f'{status}'}
     with open('save.ini', 'w') as configfile:
         save.write(configfile)
 
+## Read save file ##
 def read_save_file():
     global level
     level = (save['level']['i'])
 
+def read_name():
+    global name
+    name = (save['name']['status'])
+
 def continue_game(l, f):
     if level == l:
-        f
+        f()
     else:
         print('False')
 
+def test():
+    print('Level Test')
+
 if __name__ == '__main__':
-    save_game(3)
+    save_game(10)
     read_save_file()
-    continue_game()
+    print(level)
+    continue_game('10',test)
