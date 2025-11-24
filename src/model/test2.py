7f2f6f7ae169daa8cdea0c19f3ca5ad45f3494fa
Jorge <jorgejapao00@gmail.com>
2025-11-24 09:37:52 -0300

    entire code moved to a class

--- src/model/test2.py
+++ src/model/test2.py
@@ -2,37 +2,37 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-csvPath = r"src\data\dataset.csv"
-sentences = ["eu gosto de sorvete.", "você também gosta de sorvete?"]
-dataset = pd.read_csv(csvPath)
-datasetFrases = dataset["frase"]
-datasetPoints = dataset["label"].to_list()
+class Preparer:
+    def tokenize():
+        csvPath = r"src\data\dataset.csv"
+        dataset = pd.read_csv(csvPath)
+        datasetFrases = dataset["frase"]
+        datasetPoints = dataset["label"].to_list()
 
 
-#print(datasetFrases)
-#print(datasetPoints)
+        #print(datasetFrases)
+        #print(datasetPoints)
 
-text_vectorization_layer = tf.keras.layers.TextVectorization(
-    max_tokens=1000,
-    output_mode='int'
-)
-
-
-
-text_vectorization_layer.adapt(datasetFrases)
-output = text_vectorization_layer(datasetFrases)
-
-resultDict = output.numpy()
-convertedDict = []
-
-for internalDict in resultDict:
-    valueSum = internalDict.cumsum()[-1]
-    convertedDict.append(valueSum)
-
-
-frasesS = np.array(convertedDict, dtype=float)
-print(frasesS)
+        text_vectorization_layer = tf.keras.layers.TextVectorization(
+            max_tokens=1000,
+            output_mode='int'
+        )
 
 
 
-#print("Vectorized Output:", output.numpy())
+        text_vectorization_layer.adapt(datasetFrases)
+        output = text_vectorization_layer(datasetFrases)
+
+        resultDict = output.numpy()
+        convertedDict = []
+
+        for internalDict in resultDict:
+            valueSum = internalDict.cumsum()[-1]
+            convertedDict.append(valueSum)
+
+
+        frasesS = np.array(convertedDict, dtype=float)
+        print(frasesS)
+
+
+Preparer.tokenize()
